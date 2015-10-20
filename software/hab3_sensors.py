f4d5081de81d1000c9b8d9ed6d6db68615c85534
jbohling <bohling.jw@gmail.com>
2015-10-20 19:40:56 -0400

    polling SenseHat sensors
    polling SenseHat sensors and saving to csv file

---
+++ software/hab3_sensors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from sense_hat import sense_hat
+import csv
+import time
+import picamera
+
+sense = SenseHat()
+
+with open('sensor-data.csv','w', newline='') as f:
+    writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
+    startloop = time.time()
+
+    # Main sensor polling loop
+    while True:
+
+        for count in list(range(6)):
+            temp = sense.get_temperature()
+            pressure = sense.get_pressure()
+            humidity = sense.get_humidity()
+            cur_time = time.strftime("%H:%M:%S",time.gmtime())
+            writer.writerow([cur_time,temp,pressure,humidity])
+            time.sleep(10.00 - ((time.time() - startloop) % 10.00))
+
