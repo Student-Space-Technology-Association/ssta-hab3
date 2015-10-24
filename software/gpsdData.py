c656f36c08025a77c3e590efeaa555d67a342033
jbohling <bohling.jw@gmail.com>
2015-10-24 18:37:24 -0400

    GPS data to csv
    Write GPS data to csv file in addition to printing it to terminal

--- software/gpsdData.py
+++ software/gpsdData.py
@@ -7,6 +7,7 @@ from gps import *
 from time import *
 import time
 import threading
+import csv
 
 gpsd = None #seting the global variable
 
@@ -25,38 +26,45 @@ class GpsPoller(threading.Thread):
 		while gpsp.running:
 			gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
-if __name__ == '__main__':
-	gpsp = GpsPoller() # create the thread
-	try:
-		gpsp.start() # start it up
-		while True:
-			#It may take a second or two to get good data
-			#print gpsd.fix.latitude,', ',gpsd.fix.longitude,'	Time: ',gpsd.utc
+# Open csv file for writing GPS data
+with open('gps-data.csv','w',newline='') as f:
+	writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
+	#loop_ref_time = time.time()
 
-			os.system('clear')
+	if __name__ == '__main__':
+		gpsp = GpsPoller() # create the thread
+		try:
+			gpsp.start() # start it up
+			while True:
+				#It may take a second or two to get good data
+				#print gpsd.fix.latitude,', ',gpsd.fix.longitude,'	Time: ',gpsd.utc
 
-			print
-			print ' GPS reading'
-			print '----------------------------------------'
-			print 'latitude    ' , gpsd.fix.latitude
-			print 'longitude   ' , gpsd.fix.longitude
-			print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-			print 'altitude (m)' , gpsd.fix.altitude
-			print 'eps         ' , gpsd.fix.eps
-			print 'epx         ' , gpsd.fix.epx
-			print 'epv         ' , gpsd.fix.epv
-			print 'ept         ' , gpsd.fix.ept
-			print 'speed (m/s) ' , gpsd.fix.speed
-			print 'climb       ' , gpsd.fix.climb
-			print 'track       ' , gpsd.fix.track
-			print 'mode        ' , gpsd.fix.mode
-			print
-			print 'sats        ' , gpsd.satellites
+				os.system('clear')
 
-			time.sleep(5) #set to whatever
+				print
+				print ' GPS reading'
+				print '----------------------------------------'
+				print 'latitude    ' , gpsd.fix.latitude
+				print 'longitude   ' , gpsd.fix.longitude
+				print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+				print 'altitude (m)' , gpsd.fix.altitude
+				print 'eps         ' , gpsd.fix.eps
+				print 'epx         ' , gpsd.fix.epx
+				print 'epv         ' , gpsd.fix.epv
+				print 'ept         ' , gpsd.fix.ept
+				print 'speed (m/s) ' , gpsd.fix.speed
+				print 'climb       ' , gpsd.fix.climb
+				print 'track       ' , gpsd.fix.track
+				print 'mode        ' , gpsd.fix.mode
+				print
+				print 'sats        ' , gpsd.satellites
 
-	except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-		print "\nKilling Thread..."
-		gpsp.running = False
-		gpsp.join() # wait for the thread to finish what it's doing
-	print "Done.\nExiting."
+				writer.writerow([gpsd.utc,gpsd.fix.latitude,gpsd.fix.longitude,gpsd.fix.altitude,gpsd.fix.speed,gpsd.fix.climb,gpsd.fix.track])
+
+				time.sleep(5) #set to whatever
+
+		except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+			print "\nKilling Thread..."
+			gpsp.running = False
+			gpsp.join() # wait for the thread to finish what it's doing
+		print "Done.\nExiting."
