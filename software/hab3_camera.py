9803e63d1ee37a31b3bc1683b66bacb7bc41aff5
jbohling <bohling.jw@gmail.com>
2015-10-27 18:20:48 -0400

    temporarily removed camera code for low light exposure conditions

--- software/hab3_camera.py
+++ software/hab3_camera.py
@@ -6,13 +6,15 @@ imagedir = '/home/jbohling/'
 
 with picamera.PiCamera() as camera:
     camera.resolution = (2592,1944)
-    camera.framerate = Fraction(1,6)
-    camera.shutter_speed = 6000000
-    camera.exposure_mode = 'off'
-    camera.iso = 800
     
-    # camera.start_preview()
+    #camera.framerate = Fraction(1,6)
+    #camera.shutter_speed = 6000000
+    #camera.exposure_mode = 'off'
+    #camera.iso = 800
+
+    camera.start_preview()
     time.sleep(10)
+    
     for filename in camera.capture_continuous(imagedir + 'hab3_img{timestamp:%Y%m%d-%H%M%S}.jpg'):
         print('Captured %s' % filename)
         time.sleep(300) # Wait 5 minutes
