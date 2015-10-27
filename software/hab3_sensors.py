6282f4e6338f33d266bc3f53d963da39f825e482
jbohling <bohling.jw@gmail.com>
2015-10-27 17:59:44 -0400

    fixed indentation

--- software/hab3_sensors.py
+++ software/hab3_sensors.py
@@ -16,14 +16,13 @@ sense = SenseHat()
 # Main sensor polling loop
 while True:
 
-	temp = sense.get_temperature()
+    temp = sense.get_temperature()
     pressure = sense.get_pressure()
     humidity = sense.get_humidity()
     cur_time = time.strftime("%H:%M:%S",time.gmtime())
 
-	with open("sensehat_" + csvfilename + '.csv','a', newline='') as f:
-		writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
+    with open("sensehat_" + csvfilename + '.csv','a', newline='') as f:
+        writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
         writer.writerow([cur_time,temp,pressure,humidity])
 
     time.sleep(10.00 - ((time.time() - startloop) % 10.00))
-
