3b89d231756682694a1343633065304d37c8fd89
jbohling <bohling.jw@gmail.com>
2015-10-27 17:50:21 -0400

    write and close csv file
    Write to the csv file and close it each time we go through the loop

--- software/hab3_sensors.py
+++ software/hab3_sensors.py
@@ -5,22 +5,25 @@
 from sense_hat import SenseHat
 import csv
 import time
-import datetime
 
+
+# Make a new file each time the script runs
+csvfilename = time.strftime("%Y%m%d-%H%M%S",time.gmtime())
+
+startloop = time.time()
 sense = SenseHat()
 
-csvfilename = time.strftime("%Y%m%d-%H%M%S",time.gmtime())
-with open("sensehat_" + csvfilename + '.csv','w', newline='') as f:
-    writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
-    startloop = time.time()
+# Main sensor polling loop
+while True:
 
-    # Main sensor polling loop
-    while True:
+	temp = sense.get_temperature()
+    pressure = sense.get_pressure()
+    humidity = sense.get_humidity()
+    cur_time = time.strftime("%H:%M:%S",time.gmtime())
 
-        temp = sense.get_temperature()
-        pressure = sense.get_pressure()
-        humidity = sense.get_humidity()
-        cur_time = time.strftime("%H:%M:%S",time.gmtime())
+	with open("sensehat_" + csvfilename + '.csv','a', newline='') as f:
+		writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
         writer.writerow([cur_time,temp,pressure,humidity])
-        time.sleep(10.00 - ((time.time() - startloop) % 10.00))
+
+    time.sleep(10.00 - ((time.time() - startloop) % 10.00))
 
