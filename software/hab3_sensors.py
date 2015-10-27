4b4c2a1d2b1d735a6e832c3f9c904ad7867b5844
jbohling <bohling.jw@gmail.com>
2015-10-27 17:04:31 -0400

    removed piCamera code
    Camera code has been moved to separate script for now, so removing from
    sensor script

--- software/hab3_sensors.py
+++ software/hab3_sensors.py
@@ -3,7 +3,7 @@
 from sense_hat import SenseHat
 import csv
 import time
-import picamera
+import datetime
 
 sense = SenseHat()
 
@@ -14,15 +14,10 @@ with open('sensor-data.csv','w', newline='') as f:
     # Main sensor polling loop
     while True:
 
-        for count in list(range(6)):
-            temp = sense.get_temperature()
-            pressure = sense.get_pressure()
-            humidity = sense.get_humidity()
-            cur_time = time.strftime("%H:%M:%S",time.gmtime())
-            writer.writerow([cur_time,temp,pressure,humidity])
-            time.sleep(10.00 - ((time.time() - startloop) % 10.00))
-
-        with picamera.PiCamera() as camera:
-            camera.resolution = (1920, 1080)
-            camera.capture_continuous('img{timestamp:%H-%M-%S}.jpg')
+        temp = sense.get_temperature()
+        pressure = sense.get_pressure()
+        humidity = sense.get_humidity()
+        cur_time = time.strftime("%H:%M:%S",time.gmtime())
+        writer.writerow([cur_time,temp,pressure,humidity])
+        time.sleep(10.00 - ((time.time() - startloop) % 10.00))
 
