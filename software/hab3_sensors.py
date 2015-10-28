36d1f0bf117bbb3d0bf3eea741c31ace4c05f9bb
jbohling <bohling.jw@gmail.com>
2015-10-28 11:10:46 -0400

    Changes for readability
    Added comments and changed variable names for clarity / readability

--- software/hab3_sensors.py
+++ software/hab3_sensors.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
-# Data collection for SenseHat sensors (temperature, pressure, humidity)
+# Data collection for R.Pi SenseHat sensors (temperature, pressure, humidity)
+# High Altitude Balloon, Series 3 (HAB3)
+# Space Technology Student Association, University of Tennnessee, Knoxville, TN
+
 
 from sense_hat import SenseHat
 import csv
 import time
 
+sense = SenseHat()
 
-# Make a new file each time the script runs
+# Record to a new file each time the script runs
 csvfilename = time.strftime("%Y%m%d-%H%M%S",time.gmtime())
 
-startloop = time.time()
-sense = SenseHat()
+# Set a reference time at startup for calculating sleep duration in main loop
+ref_time = time.time()
 
 # Main sensor polling loop
 while True:
 
-    temp = sense.get_temperature()
+    temp = sense.get_temperature() # temperature from humidity sensor
     pressure = sense.get_pressure()
     humidity = sense.get_humidity()
-    cur_time = time.strftime("%H:%M:%S",time.gmtime())
+    current_time = time.strftime("%H:%M:%S",time.gmtime())
 
     with open("sensehat_" + csvfilename + '.csv','a', newline='') as f:
         writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([cur_time,temp,pressure,humidity])
+        writer.writerow([current_time,temp,pressure,humidity])
 
-    time.sleep(10.00 - ((time.time() - startloop) % 10.00))
+    time.sleep(10.00 - ((time.time() - ref_time) % 10.00))
