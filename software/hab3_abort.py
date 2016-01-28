54fd39ef2744450793ba4fb43a046da280d37c92
jbohling <bohling.jw@gmail.com>
2016-01-28 18:13:09 -0500

    initial script for abort sequence

---
+++ software/hab3_abort.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# This script implements abort/payload cutdown functionality for the HAB-III
+# in accordance with FAA requirements.
+
+import RPi.GPIO as GPIO
+import sys
+
+# Pick a GPIO pin to connect to abort relay
+abort_pin = 23
+
+# Setup GPIO as output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(abort_pin, GPIO.OUT)
+
+# Initialize output pin
+GPIO.output(abort_pin, GPIO.LOW)
+
+# Ask user to confirm abort
+abort_confirm = input('If you continue, the balloon will fall from the sky! Are you REALLY sure you want to abort? [y/n]')
+if not abort_confirm or abort_confirm[0].lower() != 'y':
+	print('Abort has been canceled.')
+	sys.exit(1)
+
+# Activate the relay to cut the balloon shrouds
+GPIO.output(abort_pin, GPIO.HIGH)
+print('The HAB3 is falling now...goodbye.')
+sys.exit()
