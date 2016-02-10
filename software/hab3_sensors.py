e5145ef1a99b534d01bb8944aa830add8052a01c
jbohling <bohling.jw@gmail.com>
2016-02-09 19:14:22 -0500

    Remove Arduino code
    Removed the code that talks to the Arduino to get data from the BMP180s
    (connected to the Arduino)

--- software/hab3_sensors.py
+++ software/hab3_sensors.py
@@ -6,20 +6,9 @@
 
 
 from sense_hat import SenseHat
-import serial
 import csv
 import time
 
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
-)
-
-#ser.open()
-#ser.isOpen()
 
 sense = SenseHat()
 
@@ -39,27 +28,11 @@ while True:
     pressure = sense.get_pressure()
     humidity = sense.get_humidity()
 
-    # Get altitude and pressure from the BMP180
-    ser.write('a')
-    raw_bmp_info = ''
-    while ser.inWaiting() > 0:
-        raw_bmp_info += ser.read(1)
-    raw_bmp_info = raw_bmp_info.split(',')
 
     # Write Sensehat data to csv
     with open("sensehat_" + csvfilename + '.csv','a', newline='') as f:
         writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
         writer.writerow([current_time,temp,pressure,humidity])
-
-    # Write BMP180 data to csv
-    with open("bmp180_" + csvfilename + '.csv','a', newline='') as f:
-        writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([raw_bmp_info[0],raw_bmp_info[1]])
-
-    # This is assuming we want a separate file for the different sensors
-    #with open("bmp180_" + csvfilename + '.csv', 'a', newline='') as f:
-    #    writer = csv.writer(f,quoting=csv.QUOTE_MINIMAL)
-    #    writer.writerow([current_time,bmp_info[0],bmp_info[1]])
 
     cur_time = time.time() # Update the 'current time'
     # Subtract the elapsed time from 10 to make each reading happen at 10 second intervals
