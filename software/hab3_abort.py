fea9714dfb0f64e4a07c6426797337b64ae62410
jbohling <bohling.jw@gmail.com>
2016-03-22 19:01:44 -0400

    change pin for Nichrome

--- software/hab3_abort.py
+++ software/hab3_abort.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 import sys
 
 # Pick a GPIO pin to connect to the transistor activating the cutdown device
-abort_pin = 23
+abort_pin = 24
 
 # Setup GPIO as output
 GPIO.setmode(GPIO.BCM)
